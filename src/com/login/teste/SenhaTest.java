fb90217819f33dedc895ef27512ff599ea53c01b
karlaviana <karlavianacosta@gmail.com>
2016-05-04 14:17:50 -0300

    Classe de teste
    melhorias na classe de testes senha

--- src/com/login/teste/SenhaTest.java
+++ src/com/login/teste/SenhaTest.java
@@ -1,19 +1,24 @@
 package com.login.teste;
-
 import static org.junit.Assert.*;
 import org.junit.Test;
 import com.login.Senha;
 
-
 public class SenhaTest extends Senha{
 	private Senha senha = new Senha();
 
-	@Test
-	public void testarSenha() {
-	assertFalse("Retorna False - senha invalido",senha.validarSenha("$%$%"));
-	assertFalse("Reterona False- senha em Branco",senha.validarSenha(""));
-	assertFalse("Reterona False- senha Invalido",senha.validarSenha("2345"));
-	assertTrue("Retorna True - senha correto",senha.ValidarsenhaIncorreto("123"));
+		@Test
+		public void testarSenhaInvalida() {
+		assertFalse("Retorna False - senha invalido",senha.validarSenha("$%$%"));
+		assertFalse("Reterona False- senha em Branco",senha.validarSenha(""));
+		assertFalse("Reterona False- senha Invalido",senha.validarSenha("2345"));
+		
 
-	} 
-}
+		} 
+		@Test
+		public void testarSenhaValida(){
+		assertTrue("Retorna True - senha correto",senha.validarSenha("123"));
+		}
+		
+		
+		
+	}
