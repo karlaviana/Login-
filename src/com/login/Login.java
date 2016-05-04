cec1c34d207f2a5f4749eb82f9588e5de50a0df9
karlaviana <karlavianacosta@gmail.com>
2016-05-04 14:00:05 -0300

    novo código do login Melhorado
    Novo Codigo do login melhorada :)

--- src/com/login/Login.java
+++ src/com/login/Login.java
@@ -2,10 +2,10 @@ package com.login;
 
 public class Login {
 	public boolean validarLogin(String login) {
+		if ((login== null) || (login.length()!=3)|| (login.isEmpty())){
 		return false;
 	}
-	public boolean ValidarLoginIncorreto(String login){
 		return true;
 	}
-
+	
 }
